43b3b85db6e9f4f8a4101fbedc187ead3644a9b1
plimdahl <lassepost@hotmail.dk>
2022-10-13 11:20:25 +0200

    service check first version

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,13 +4,45 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+echo -e "Checking versions:\n"
+
+echo  "cinead-d `dpkg -s cinead-d | grep -i version`"
+echo "cinead-p `dpkg -s cinead-p | grep -i version`"
+echo "cinemataztic-config `dpkg -s cinemataztic-config | grep -i version`"
+echo "cinemataztic-player `dpkg -s cinemataztic-player | grep -i version`"
+echo -e "cinematazticio24 `dpkg -s cinematazticio24 | grep -i version`\n"
+
 echo -e "checking services:\n"
 
-i=$(ps -eaf | grep -i cups | sed '/^$/d' | wc -l); echo $i
+i=$(ps -eaf | grep -i cinead-d | sed '/^$/d' | wc -l); echo $i
 case $i in
-    0) echo -e "${RED}The mysql service is not running.";;
-    1) echo -e "${RED}The mysql Service has failed. You may need to restart the service\n";;
-    2) echo -e "${GREEN}The mysql service is running. No problems detected.\n";;
+    0) echo -e "${RED}The cinead-d service is not running.\n";;
+    1) echo -e "${RED}The cinead-d Service has failed. You may need to restart the service\n";;
+    2) echo -e "${GREEN}The cinead-d service is running. No problems detected.\n";;
+esac
+
+i=$(ps -eaf | grep -i cinead-p | sed '/^$/d' | wc -l); echo $i
+case $i in
+    0) echo -e "${RED}The cinead-p service is not running.\n";;
+    1) echo -e "${RED}The cinead-p Service has failed. You may need to restart the service\n";;
+    2) echo -e "${GREEN}The cinead-p service is running. No problems detected.\n";;
+esac
+
+i=$(ps -eaf | grep -i cinemataztic-player | sed '/^$/d' | wc -l); echo $i
+case $i in
+    0) 
+    echo -e "${RED}The cinemataztic-player service is not running.\n";;
+    1) 
+    echo -e "${RED}The cinemataztic-player Service has failed. You may need to restart the service\n";;
+    2) 
+    echo -e "${GREEN}The cinemataztic-player service is running. No problems detected.\n";;
+esac
+
+i=$(ps -eaf | grep -i cinematazticio24 | sed '/^$/d' | wc -l); echo $i
+case $i in
+    0) echo -e "${RED}The cinematazticio24 service is not running.\n";;
+    1) echo -e "${RED}The cinematazticio24 Service has failed. You may need to restart the service\n";;
+    2) echo -e "${GREEN}The cinematazticio24 service is running. No problems detected.\n";;
 esac
 
 ping -c1 api.cinemataztic.com > /dev/null
--- versioncheck.sh
+++ versioncheck.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+
+echo -e "Checking versions:\n"
+
+echo  "cinead-d `dpkg -s cinead-d | grep -i version`"
+echo "cinead-p `dpkg -s cinead-p | grep -i version`"
+echo "cinemataztic-config `dpkg -s cinemataztic-config | grep -i version`"
+echo "cinemataztic-player `dpkg -s cinemataztic-player | grep -i version`"
+echo "cinematazticio24 `dpkg -s cinematazticio24 | grep -i version`"
+
+
+
