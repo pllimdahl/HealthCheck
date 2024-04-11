e78f2b72990c7967f721ba48fd7d5861cdbaf0bb
plimdahl <lassepost@hotmail.dk>
2024-04-11 13:07:56 +0200

    add stuff

--- oneliner.sh
+++ oneliner.sh
@@ -1,10 +1,7 @@
 # Initial newline and header
 echo -e "\nSTARTING HEALTH CHECK\n"
 
-# Screen resolution and output for player user
-echo -e "SCREEN RES AND OUTPUT:\n"
-sudo runuser -l player -c 'export DISPLAY=:0 && xrandr | grep HDMI-0'
-echo -e "\nCHECKING NETWORK:\n"
+
 
 # IP and MAC address
 echo -e "IP and MAC: $(hostname --ip-address)\n"
@@ -15,7 +12,7 @@ curl -s --head --request GET http://api.cinemataztic.com > /dev/null 2>&1 && ech
 
 # Checking versions
 echo -e "CHECKING VERSIONS:\n"
-echo "dch-p   $(dpkg -s dch | grep -i version)"
+echo "dch-p   $(dpkg -s dch-p | grep -i version)"
 echo "cinead-d $(dpkg -s cinead-d | grep -i version)"
 echo "cinead-p $(dpkg -s cinead-p | grep -i version)"
 echo "cinegame-d    $(dpkg -s cinegame-d | grep -i version)"
@@ -24,11 +21,27 @@ echo "cinecard-d    $(dpkg -s cinecard-d | grep -i version)"
 echo "cinecard-p    $(dpkg -s cinecard-p | grep -i version)"
 echo -e "cinematazticio24 $(dpkg -s cinematazticio24 | grep -i version)\n"
 
+echo "nodejs    $(dpkg -s nodejs | grep -i version)"
+
 # Checking services
 echo -e "CHECKING SERVICES:\n"
-echo "dch-p status: $(systemctl is-active dch.service)"
+echo "dch-p status: $(systemctl is-active dch-p.service)"
 echo "cinead-d status: $(systemctl is-active cinead-d.service)"
 echo "cinegame-d status: $(systemctl is-active cinegame-d.service)"
 echo "cinecard-d status: $(systemctl is-active cinecard-d.service)"
 echo "cinematazticio24 status: $(systemctl is-active cinematazticio24.service)"
 echo -e "\n"
+
+# Check NVIDIA driver
+echo -e "CHECKING NVIDIA DRIVER:\n"
+if nvidia-smi &>/dev/null; then
+    echo -e "NVIDIA driver is installed and active.\n"
+else
+    echo -e "NVIDIA driver is not installed.\n"
+fi
+
+# Screen resolution and output for player user
+echo -e "SCREEN RES AND OUTPUT:\n"
+sudo runuser -l player -c 'export DISPLAY=:0 && xrandr | grep HDMI-0'
+echo -e "\nCHECKING NETWORK:\n"
+
